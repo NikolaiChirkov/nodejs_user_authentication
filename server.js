e4c9348208b81d5c61378da4b0b9847f44790c02
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-20 14:09:45 +0300

    login

--- server.js
+++ server.js
@@ -24,4 +24,20 @@ app.post('/users', async (req,res) => {
     }
 });
 
+app.post('/users/login', async (req, res) => {
+    const user = users.find (user => user.name = req.body.name);
+    if (user === null) {
+        return res.status(400).send('Cannot find user');
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send("Success");
+        } else {
+            res.send("Not Allowd");
+        }
+    } catch {
+        res.status(500).send();
+    }
+})
+
 app.listen(3000);
